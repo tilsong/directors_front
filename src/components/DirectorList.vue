86adc04c9681d115bc6e954bf541d87b61de57a5
tilsong <cnsong0229@gmail.com>
2023-06-30 18:29:23 +0900

    [#1] 검색 필터 추가 (수정 필요)

--- src/components/DirectorList.vue
+++ src/components/DirectorList.vue
@@ -3,24 +3,38 @@
     <v-responsive class="align-center fill-height">
       <h3 class="text-h4 font-weight-bold text-center">디렉터 찾기</h3>
       <div class="py-5" />
-      <div class="text-center text-h5" id="filter">
-        <div>필터</div>
-        <div>
-          <!-- (로그인x -> uncheck 모드, 로그인 시 유저 지역 및 범위 정도 설정할 수 있도록 하기) -->
-          <div>지역 설정 </div>
-          <!-- 선택: 콤보 박스 사용  -->
-          <div>전문 분야 선택</div>
+      <div class="text-center text-h6" id="filter">
+        <div id="filter-title">검색 필터</div>
+        <div id="filter-list">
+            <div>지역 설정 </div>
+            <!-- (로그인x -> uncheck 모드, 로그인 시 유저 지역 및 범위 정도 설정할 수 있도록 하기) -->
+
+            <v-combobox
+            v-model="property"
+            label="분야"
+            :items="specialtyProperty"
+            variant="solo"
+          />
+
+
           <!-- 선택: 체크 박스 사용  -->
-          <div>스케줄 유무 선택</div>
-          <!-- 선택: input form 사용  -->
-          <div>검색</div>
+          <v-switch
+          color="primary"
+          :model-value="hasSchedule"
+          label="스케줄"
+          class="custom-switch"          
+          />
+
+          <div class="custom-textfield">
+            <input type="text" class="custom-textfield-input" placeholder="검색어를 입력하세요">
+          </div>
         </div>
       </div>
       
       <v-row class="d-flex align-center justify-center " cols="8">
         <div class="item-list"  ref="itemList">
           <v-col  v-for="(item, index) in directorList" :key="index">
-            <div class="item-wrapper  a">
+            <div class="item-wrapper  a" @click="moveDirectorPage(item.id)">
               <div class="item-content">
                 <div class="item-image">
                   <img :src="item.image" alt="Director Image">
@@ -40,8 +54,70 @@
 </template>
 
 <style scoped>
+.custom-textfield {
+  position: relative;
+  width: 200px;
+}
+
+.custom-textfield-input {
+  width: 100%;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  font-size: 14px;
+}
+
+.custom-textfield-label {
+  position: absolute;
+  top: 50%;
+  left: 10px;
+  transform: translateY(-50%);
+  pointer-events: none;
+  transition: transform 0.3s, font-size 0.3s;
+  font-size: 14px;
+  color: #999;
+}
+
+.custom-textfield-input:focus + .custom-textfield-label,
+.custom-textfield-input:not(:placeholder-shown) + .custom-textfield-label {
+  transform: translateY(-100%) scale(0.8);
+  font-size: 12px;
+  color: #555;
+}
+
+.v-input--switch__track {
+    border-radius: 25px;
+    width: 68px;
+    height: 29px;
+    top: -2px;
+}
+
+.v-input--switch__thumb {
+  left: 6px;
+}
+
+.custom-switch .v-input__slot .v-label {
+  left: 6px !important
+}
+
+.v-input--selection-controls__ripple {
+  height: 0;
+  width: 0
+}
+#filter-list {
+  display: flex;
+  justify-content: flex-start
+}
+#filter-list > div {
+  margin-right: 0px;
+}
+#filter-title {
+  text-align: left;
+  margin-bottom: 10px;
+}
 #filter {
-  border: 1px solid blue;
+  font-weight: bold;
+  /* border: 1px solid blue; */
 }
 .specialty {
   font-size: 20px;
@@ -77,6 +153,7 @@
 }
 
 .item-details {
+  /* float: right; */
   flex: 1;
 }
 
@@ -92,20 +169,66 @@
 export default {
   data: () => ({
     distance: 3,
-    property: "프로그래밍",
+    property: null,
     hasSchedule: false,
     searchText: null,
     page: 1,
+    specialtyProperty: [
+    "전체",
+    "프로그래밍",
+    "디자인",
+    "헬스",
+    "엔지니어링",
+    "교육",
+    "인적자원",
+    "경영지원",
+    "건설",
+    "영업",
+    "생산",
+    "마케팅",
+    "법무",
+    "미디어",
+    "의료",
+    "서비스",
+    "금융",
+    "물류",
+    "무역",
+    "전문직",
+    "생활",
+    "육아",
+    "여행",
+    "예술",
+    "기타",
+    ]
   }),
   mounted() {
-    this.fetchData();
+    this.fetchNewData();
     window.addEventListener("scroll", this.handleScroll);
   },
   beforeUnmount() {
     window.removeEventListener("scroll", this.handleScroll);
   },
   methods: {
+    moveDirectorPage(id) {
+      console.log('moveDirectorPage: ', id);
+    },
+    fetchNewData() {
+      if (this.property === "전체") {
+        this.property = null;
+      }
+      this.$store.dispatch('fetchNewDirectorList', {
+        "distance": this.distance,
+        "property": this.property,
+        "hasSchedule": this.hasSchedule,
+        "searchText": this.searchText,
+        "page": this.page,
+        "size": 20,
+      })
+    },
     fetchData() {
+      if (this.property === "전체") {
+        this.property = null;
+      }
       this.$store.dispatch('fetchDirectorList', {
         "distance": this.distance,
         "property": this.property,
@@ -134,7 +257,6 @@ export default {
   computed: {
     directorList() {
       let list = this.$store.getters.getDirectorList;
-      console.log('lisddddt', list)
       return Array.isArray(list) ? list : [];
     },
     isLoading() {
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore } from 'vuex';
 import apiClient from '../api';
 
+
 const store = createStore({
   state: {
     directorList: [],
@@ -10,42 +11,32 @@ const store = createStore({
     setLoading(state, isLoading) {
       state.loading = isLoading;
     },
-    setDirectorList(state, data) {
-      const list = [];
-      for (let i = 0; i < data.length; i++) {
-        let specialty = "";
-        for (let j = 0; j < data[i].specialtyList.length; j++) {
-            specialty += data[i].specialtyList[j].property;
-            if (j != data[i].specialtyList.length - 1) {
-              specialty += (', ');
-            }
-        }
-
-        if (data[i].specialtyList.length > 1) {
-          data[i].specialtyList[0].description += `\n 그 외 ${data[i].specialtyList.length}개의 전문 분야 소유.`;
-        }
-
-        let description = data[i].specialtyList[0].description;
-        if (description.length > 100) {
-          description = description.substring(0, 100) + ' ... '; // 문자열의 처음부터 30글자까지 추출
-        }
-
-        list.push({
-          image: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-          name: data[i].name,
-          specialty,
-          description,
-        });
-      }
-      if (state.directorList.length != 0) {
-        state.directorList = state.directorList.concat(list);
-      } else {
-        state.directorList = list;
-      }
-
+    async setNewDirectorList(state, data) {
+      const list = await setDirectors(data);
+      state.directorList = list;
+    },
+    async setDirectorList(state, data) {
+      const list = await setDirectors(data);
+      state.directorList = state.directorList.concat(list);
     },
   },
   actions: {
+    async fetchNewDirectorList({ commit, state}, requestData) {
+      if (state.loading) {
+        return; 
+      }
+      commit('setLoading', true);
+
+      try {
+        const res = await fetchDirectors(requestData);
+        commit('setNewDirectorList', res);
+      } catch (error) {
+        console.error(error);
+      }finally {
+        commit('setLoading', false); // 로딩 상태 해제
+      }
+      
+    },
     async fetchDirectorList({ commit, state }, requestData) {
       if (state.loading) {
         return; 
@@ -53,22 +44,7 @@ const store = createStore({
       commit('setLoading', true);
 
       try {
-        const response = await apiClient.post('/user/director/list', requestData);
-        const res = response.data;
-
-        if (requestData.property != null) {
-          for (let i = 0; i < res.length; i++) {
-            const specialtyList = res[i].specialtyList;
-
-            for (let j = 0; j < specialtyList.length; j++) {
-              if (specialtyList[j].property == requestData.property) {
-                res[i].specialtyList = [specialtyList[j]];
-                break;
-              }
-            }
-          }
-        }
-
+        const res = await fetchDirectors(requestData);
         commit('setDirectorList', res);
       } catch (error) {
         console.error(error);
@@ -83,5 +59,55 @@ const store = createStore({
     },
   },
 });
+
+const fetchDirectors = async (requestData) => {
+  const response = await apiClient.post('/user/director/list', requestData);
+  const res = response.data;
+
+  if (requestData.property != null) {
+    for (let i = 0; i < res.length; i++) {
+      const specialtyList = res[i].specialtyList;
+
+      for (let j = 0; j < specialtyList.length; j++) {
+        if (specialtyList[j].property == requestData.property) {
+          res[i].specialtyList = [specialtyList[j]];
+          break;
+        }
+      }
+    }
+  }
+  return res;
+}
+const setDirectors = async (data) => {
+  const list = [];
+
+  for (let i = 0; i < data.length; i++) {
+    let specialty = "";
+    for (let j = 0; j < data[i].specialtyList.length; j++) {
+        specialty += data[i].specialtyList[j].property;
+        if (j != data[i].specialtyList.length - 1) {
+          specialty += (', ');
+        }
+    }
+
+    let description = data[i].specialtyList[0].description;
+    if (description.length > 100) {
+      description = description.substring(0, 100) + ' ... ';
+    }
+    if (data[i].specialtyList.length > 1) {
+      description += `\n 그 외 ${data[i].specialtyList.length}개의 전문 분야 소유.`;
+    }
+
+    list.push({
+      image: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+      name: data[i].name,
+      id: data[i].id,
+      specialty,
+      description,
+    });
+  }
+
+  return list;
+}
 
 export default store;
