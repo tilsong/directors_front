806ceb59474163805145869f936ef905ba828663
tilsong <cnsong0229@gmail.com>
2023-07-21 17:30:17 +0900

    [#2] 앱 바에 버튼 추가

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,23 @@
 <template>
   <v-app-bar flat>
-    <v-app-bar-title>
-      <div class="v-toolbar__prepend">
-        <div>
-          <v-img height="30" src="@/assets/di_logo_no_text.png" />
-        </div>
-        <div class="ml-2" id="app-bar-title">Directors</div>
-      </div>
-    </v-app-bar-title>
+    <template v-slot:prepend>
+      <v-img height="30" src="@/assets/di_logo_no_text.png" />
+    </template>
+    <div class="ml-2" id="app-bar-title">Directors</div>
+
+    <v-spacer></v-spacer>
+
+    <v-btn 
+    prepend-icon="mdi-account-circle" 
+    color="deep-purple-darken-3" 
+    elevation="1" 
+    style="margin-right: 7px;"
+    >
+      로그인
+    </v-btn>
+    <v-btn prepend-icon="mdi-account-plus-outline" color="yellow-darken-2" elevation="1" >
+      회원가입
+    </v-btn>
   </v-app-bar>
 </template>
 
